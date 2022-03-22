459ee6e30195675f260efaa4c5e46d6c8ebecaab
mmuniz75 <muniz_marcelo@hotmail.com>
2022-03-22 16:58:53 -0300

    open dialog on fish selection

--- src/app/aquarium/aquarium.component.ts
+++ src/app/aquarium/aquarium.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit,ViewChildren, ElementRef, QueryList } from '@angular/core';
 import { Router } from '@angular/router';
 
+declare var window: any;
+
 @Component({
   selector: 'app-aquarium',
   templateUrl: './aquarium.component.html'
 })
 export class AquariumComponent implements OnInit {
 
+  fishDialog: any;
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.fishDialog = new window.bootstrap.Modal(
+      document.getElementById('fishDialog')
+    );
   }
 
   openHardscape() {
     this.router.navigate(['/hardscape']);
+  }
+
+  openFishDialog(){
+    this.fishDialog.show();
   }
 
 }
