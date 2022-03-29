35f849c411bdc649a91293db3ee1889b6d161beb
mmuniz75 <muniz_marcelo@hotmail.com>
2022-03-29 16:01:11 -0300

    icon ios and android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var window: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aquario';
+  navegador = 'Chrome';
+
+
+  openIOSDialog(navegador : string){
+    this.navegador = navegador
+    let dialog = new window.bootstrap.Modal(
+      document.getElementById('iosInstalation')
+    );
+    dialog.show()  
+
+  }
 }
