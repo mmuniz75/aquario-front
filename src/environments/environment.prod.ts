972e55ec12489d901e90c89a62f59c907bc16bb5
mmuniz75 <muniz_marcelo@hotmail.com>
2022-12-09 09:57:31 -0300

    fly ip server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  SERVER_URL : "https://fish-selector.herokuapp.com/aquarium",
+  SERVER_URL : "https://fish-selector.fly.dev/aquarium",
+  //SERVER_URL : "https://fish-selector.herokuapp.com/aquarium",
   //SERVER_URL : "http://localhost:8080/aquarium",
 
 };
